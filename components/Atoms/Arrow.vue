ac81d70a702fe003f5c1e3a0a5ab1e6f5d6a3b11
benounnas oussama <benounnas.oussama@gmail.com>
2022-10-31 17:28:01 +0100

    init

---
+++ components/Atoms/Arrow.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+
+interface Props {
+  disabled?: boolean;
+  bgColor?: string;
+  textColor?: string;
+  focusColor: string;
+}
+
+const emit = defineEmits(["click"]);
+
+const props = withDefaults(defineProps<Props>(), {
+  disabled: false,
+});
+</script>
+<template>
+  <button
+    class="w-[2.75rem] h-[2.75rem] py-0 px-[0.0625rem] flex items-center justify-center hover:shadow-lg bg-zinc-200 transition-colors delay-5 ease-out"
+    :class="[
+      bgColor ?? 'hover:bg-zinc-900 ',
+      textColor ?? 'hover:text-white',
+      focusColor,
+    ]"
+  >
+    <Icon icon="fa6-solid:arrow-left-long" />
+  </button>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="p-5">
+    <AtomsArrow
+      :focus-color="'active:outline-none active:ring active:ring-slate-400 active:bg-zinc-700'"
+    />
+  </div>
+</template>
